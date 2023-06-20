cd50c2ae46f9dcb337ac981782f4d4f8936860a0
Rizwan Ansar <131580981+RizwanAnsar2004@users.noreply.github.com>
2023-06-20 07:20:43 +0500

    fixing bugs related to colours and MESSAGE DIALOG BOX

--- Algo-Visualizer/src/algo/visualizer/Mypanel.java
+++ Algo-Visualizer/src/algo/visualizer/Mypanel.java
@@ -23,25 +23,31 @@ public class Mypanel{
 
     //SWING COMPONENTS
     private JButton shuffle, insertionDec, bubbleDec, quickDec, mergeDec, selectionDec, shellDec;
-    private JButton insertion, bubble, quick, merge, selection, shell;
+    private JButton insertion, bubble, quick, merge, selection, shell,stop;
     private JPanel panel;
+    private Thread sortingThread;
+    private volatile boolean isSorting = false;
+
     
     //THEME BOOLEANS
     private boolean isTheme1 = true;
     private boolean isTheme2 = false;
     private boolean isTheme3 = false;
     private boolean isTheme4 = false;
+    private boolean stopSorting = false;
+
 
     //SORTING SPEED SETTER
-    private int sortingSpeed = 100;
+    private int sortingSpeed = 200;
 
     //PANEL CONSTRUCTOR
     public Mypanel() {
-        this.width = 1500;
+        this.width = 1575;
         this.height = 700;
         this.size = 200;
         this.bar_height = new float[size];
         this.bar_width = (float) 5.0;
+        this.sortingThread = null;
     }
 
     //GETTER/SETTERS
@@ -132,10 +138,27 @@ public class Mypanel{
         }
         panel.repaint();
     }
+    // Terminate the current sorting thread if it is running
+    private void terminateSortingThread() {
+        stopSorting = true;
+        if (sortingThread != null && sortingThread.isAlive()) {
+            sortingThread.interrupt();
+            sortingThread = null;
+        }
+    }
+    private void stopSorting() {
+        if (isSorting) {
+            isSorting = false;
+            terminateSortingThread();
+            init_bar_height(); // Reset bar heights
+                panel.repaint(); // Repaint the panel to show the initial state
+        }
+    }
+
     //INITIALIZE THE BUTTONS THAT SORT THE ARRAY IN ACSENDING ORDER
     public void ascendingOrderAlgoButtons() {
         shuffle = new JButton("SHUFFLE");
-        shuffle.setBounds(1075, 50, 150, 45);
+        shuffle.setBounds(1200, 50, 200, 45);
         shuffle.setVisible(true);
         shuffle.setBorder(new LineBorder(Color.RED, 3));
         shuffle.setFont(new Font("Arial Black", Font.PLAIN, 13));
@@ -154,12 +177,12 @@ public class Mypanel{
         insertion.setBorder(new LineBorder(Color.RED, 3));
         insertion.setForeground(Color.WHITE);
         insertion.setBackground(Color.black);
-        insertion.addActionListener((ActionEvent e) -> {
-            shuffler();
-            new InsertionSort(panel,this).sort(bar_height);
-        });
         insertion.setFont(myFont);
         panel.add(insertion);
+        insertion.addActionListener((ActionEvent e) -> {
+        shuffler();
+        new InsertionSort(panel, this).sort(bar_height);
+        });
 
         bubble = new JButton("BUBBLE SORT");
         bubble.setBounds(1075, 210, 150, 45);
@@ -168,11 +191,11 @@ public class Mypanel{
         bubble.setForeground(Color.WHITE);
         bubble.setBackground(Color.black);
         bubble.setFont(myFont);
+        panel.add(bubble);
         bubble.addActionListener((ActionEvent e) -> {
             shuffler();
             new BubbleSort(panel, this).sort(bar_height);
         });
-        panel.add(bubble);
 
         merge = new JButton("MERGE SORT");
         merge.setBounds(1075, 290, 150, 45);
@@ -181,11 +204,12 @@ public class Mypanel{
         merge.setForeground(Color.WHITE);
         merge.setBackground(Color.black);
         merge.setFont(myFont);
+        panel.add(merge);
         merge.addActionListener((ActionEvent e) -> {
-            shuffler();
+                shuffler();
             new MergeSort(panel, this).sort(bar_height);
         });
-        panel.add(merge);
+        
 
         quick = new JButton("QUICK SORT");
         quick.setBounds(1075, 370, 150, 45);
@@ -194,11 +218,11 @@ public class Mypanel{
         quick.setForeground(Color.WHITE);
         quick.setBackground(Color.black);
         quick.setFont(myFont);
+        panel.add(quick);
         quick.addActionListener((ActionEvent e) -> {
-            shuffler();
+                shuffler();
             new QuickSort(panel, this).sort(bar_height);
         });
-        panel.add(quick);
 
         selection = new JButton("SELECTION SORT");
         selection.setBounds(1075, 450, 150, 45);
@@ -207,11 +231,11 @@ public class Mypanel{
         selection.setForeground(Color.WHITE);
         selection.setBackground(Color.black);
         selection.setFont(myFont);
+        panel.add(selection);
         selection.addActionListener((ActionEvent e) -> {
-            shuffler();
+                shuffler();
             new SelectionSort(panel, this).sort(bar_height);
         });
-        panel.add(selection);
 
         shell = new JButton("SHELL SORT");
         shell.setBounds(1075, 530, 150, 45);
@@ -220,11 +244,26 @@ public class Mypanel{
         shell.setForeground(Color.WHITE);
         shell.setBackground(Color.black);
         shell.setFont(myFont);
+        panel.add(shell);
         shell.addActionListener((ActionEvent e) -> {
-            shuffler();
+                shuffler();
             new ShellSort(panel, this).sort(bar_height);
         });
-        panel.add(shell);
+
+        stop = new JButton("STOP");
+        stop.setBounds(1200, 610, 200, 45);
+        stop.setVisible(true);
+        stop.setBorder(new LineBorder(Color.RED, 3));
+        stop.setFont(new Font("Arial Black", Font.PLAIN, 13));
+        stop.setForeground(Color.WHITE);
+        stop.setBackground(Color.black);
+        stop.setFont(myFont);
+        stop.addActionListener((ActionEvent e) -> {
+                    sortingThread.interrupt(); 
+        });
+        panel.add(stop);
+
+        
     }
     
     //INITIALIZE BUTTONS THAT SORT THE ARRAY IN DECENDING ORDER
@@ -235,12 +274,17 @@ public class Mypanel{
         insertionDec.setBorder(new LineBorder(Color.RED, 3));
         insertionDec.setForeground(Color.WHITE);
         insertionDec.setBackground(Color.black);
+        panel.add(insertionDec);
         insertionDec.addActionListener((ActionEvent e) -> {
-            shuffler();
+            stopSorting(); 
+            sortingThread = new Thread(() -> {
+                shuffler();
             new DecInsertionSort(panel,this).sort(bar_height);
         });
+            sortingThread.start();
+        });
         insertionDec.setFont(myFont);
-        panel.add(insertionDec);
+        
         
         bubbleDec = new JButton("BUBBLE SORT (DECENDING)");
         bubbleDec.setBounds(1250, 210, 250, 45);
@@ -249,11 +293,16 @@ public class Mypanel{
         bubbleDec.setForeground(Color.WHITE);
         bubbleDec.setBackground(Color.black);
         bubbleDec.setFont(myFont);
+        panel.add(bubbleDec);
         bubbleDec.addActionListener((ActionEvent e) -> {
-            shuffler();
+            stopSorting(); 
+            sortingThread = new Thread(() -> {
+                shuffler();
             new DecBubbleSort(panel, this).sort(bar_height);
         });
-        panel.add(bubbleDec);
+            sortingThread.start();
+        });
+        
 
         mergeDec = new JButton("MERGE SORT (DECENDING)");
         mergeDec.setBounds(1250, 290, 250, 45);
@@ -262,11 +311,16 @@ public class Mypanel{
         mergeDec.setForeground(Color.WHITE);
         mergeDec.setBackground(Color.black);
         mergeDec.setFont(myFont);
+        panel.add(mergeDec);
         mergeDec.addActionListener((ActionEvent e) -> {
-            shuffler();
+            stopSorting(); 
+            sortingThread = new Thread(() -> {
+                shuffler();
             new DecMergeSort(panel, this).sort(bar_height);
         });
-        panel.add(mergeDec);
+            sortingThread.start();
+        });
+        
 
         quickDec = new JButton("QUICK SORT (DECENDING)");
         quickDec.setBounds(1250, 370, 250, 45);
@@ -275,11 +329,16 @@ public class Mypanel{
         quickDec.setForeground(Color.WHITE);
         quickDec.setBackground(Color.black);
         quickDec.setFont(myFont);
+        panel.add(quickDec);
         quickDec.addActionListener((ActionEvent e) -> {
-            shuffler();
+            stopSorting(); 
+            sortingThread = new Thread(() -> {
+                shuffler();
             new DecQuickSort(panel, this).sort(bar_height);
         });
-        panel.add(quickDec);
+            sortingThread.start();
+        });
+        
 
         selectionDec = new JButton("SELECTION SORT (DECENDING)");
         selectionDec.setBounds(1250, 450, 250, 45);
@@ -288,11 +347,16 @@ public class Mypanel{
         selectionDec.setForeground(Color.WHITE);
         selectionDec.setBackground(Color.black);
         selectionDec.setFont(myFont);
+        panel.add(selectionDec);
         selectionDec.addActionListener((ActionEvent e) -> {
-            shuffler();
+            stopSorting();
+            sortingThread = new Thread(() -> {
+                shuffler();
             new DecSelectionSort(panel, this).sort(bar_height);
         });
-        panel.add(selectionDec);
+            sortingThread.start();
+        });
+        
 
         shellDec = new JButton("SHELL SORT (DECENDING)");
         shellDec.setBounds(1250, 530, 250, 45);
@@ -301,19 +365,25 @@ public class Mypanel{
         shellDec.setForeground(Color.WHITE);
         shellDec.setBackground(Color.black);
         shellDec.setFont(myFont);
+        panel.add(shellDec);
         shellDec.addActionListener((ActionEvent e) -> {
-            shuffler();
+           stopSorting(); 
+            sortingThread = new Thread(() -> {
+                shuffler();
             new DecShellSort(panel, this).sort(bar_height);
         });
-        panel.add(shellDec);
+            sortingThread.start();
+        });
+        
     }
     
+        
     // to create bar height
     public void init_bar_height() {
         float interval = (float) height / size;
         for (int i = 0; i < size; i++) {
             bar_height[i] = i * interval;
-        }
+                }
     }
 
     public int getSortingSpeed() {
@@ -360,6 +430,38 @@ public class Mypanel{
             isTheme2 = false;
             isTheme3 = false;
             isTheme4 = false;
+            
+            shuffle.setBorder(new LineBorder(Color.RED, 3));
+           insertionDec.setBorder(new LineBorder(Color.RED, 3));
+           bubbleDec.setBorder(new LineBorder(Color.RED, 3));
+           quickDec.setBorder(new LineBorder(Color.RED, 3));
+           mergeDec.setBorder(new LineBorder(Color.RED, 3));
+           selectionDec.setBorder(new LineBorder(Color.RED, 3));
+           shellDec.setBorder(new LineBorder(Color.RED, 3));
+            insertion.setBorder(new LineBorder(Color.RED, 3));
+            bubble.setBorder(new LineBorder(Color.RED, 3));
+            quick.setBorder(new LineBorder(Color.RED, 3));
+            merge.setBorder(new LineBorder(Color.RED, 3));
+            selection.setBorder(new LineBorder(Color.RED, 3));
+            shell.setBorder(new LineBorder(Color.RED, 3));
+            stop.setBorder(new LineBorder(Color.RED, 3));
+
+            shuffle.setBackground(Color.BLACK);
+           insertionDec.setBackground(Color.BLACK);
+           bubbleDec.setBackground(Color.BLACK);
+           quickDec.setBackground(Color.BLACK);
+           mergeDec.setBackground(Color.BLACK);
+           selectionDec.setBackground(Color.BLACK);
+           shellDec.setBackground(Color.BLACK);
+            insertion.setBackground(Color.BLACK);
+            bubble.setBackground(Color.BLACK);
+            quick.setBackground(Color.BLACK);
+            merge.setBackground(Color.BLACK);
+            selection.setBackground(Color.BLACK);
+            shell.setBackground(Color.BLACK);
+            stop.setBackground(Color.BLACK);
+            
+            panel.repaint();
         });
         
         theme2.addActionListener((ActionEvent e) -> {
@@ -367,6 +469,36 @@ public class Mypanel{
            isTheme1 = false;
            isTheme3 = false;
            isTheme4 = false;
+           shuffle.setBorder(new LineBorder(Color.GREEN, 3));
+           insertionDec.setBorder(new LineBorder(Color.GREEN, 3));
+           bubbleDec.setBorder(new LineBorder(Color.GREEN, 3));
+           quickDec.setBorder(new LineBorder(Color.GREEN, 3));
+           mergeDec.setBorder(new LineBorder(Color.GREEN, 3));
+           selectionDec.setBorder(new LineBorder(Color.GREEN, 3));
+           shellDec.setBorder(new LineBorder(Color.GREEN, 3));
+            insertion.setBorder(new LineBorder(Color.GREEN, 3));
+            bubble.setBorder(new LineBorder(Color.GREEN, 3));
+            quick.setBorder(new LineBorder(Color.GREEN, 3));
+            merge.setBorder(new LineBorder(Color.GREEN, 3));
+            selection.setBorder(new LineBorder(Color.GREEN, 3));
+            shell.setBorder(new LineBorder(Color.GREEN, 3));
+            stop.setBorder(new LineBorder(Color.GREEN, 3));
+            
+            shuffle.setBackground(Color.BLACK);
+           insertionDec.setBackground(Color.BLACK);
+           bubbleDec.setBackground(Color.BLACK);
+           quickDec.setBackground(Color.BLACK);
+           mergeDec.setBackground(Color.BLACK);
+           selectionDec.setBackground(Color.BLACK);
+           shellDec.setBackground(Color.BLACK);
+            insertion.setBackground(Color.BLACK);
+            bubble.setBackground(Color.BLACK);
+            quick.setBackground(Color.BLACK);
+            merge.setBackground(Color.BLACK);
+            selection.setBackground(Color.BLACK);
+            shell.setBackground(Color.BLACK);
+            stop.setBackground(Color.BLACK);  
+            panel.repaint();
         });
         
         theme3.addActionListener((ActionEvent e) -> {
@@ -374,6 +506,36 @@ public class Mypanel{
             isTheme1 = false;
             isTheme2 = false;
             isTheme4 = false;
+            shuffle.setBorder(new LineBorder(Color.BLACK, 3));
+           insertionDec.setBorder(new LineBorder(Color.BLACK, 3));
+           bubbleDec.setBorder(new LineBorder(Color.BLACK, 3));
+           quickDec.setBorder(new LineBorder(Color.BLACK, 3));
+           mergeDec.setBorder(new LineBorder(Color.BLACK, 3));
+           selectionDec.setBorder(new LineBorder(Color.BLACK, 3));
+           shellDec.setBorder(new LineBorder(Color.BLACK, 3));
+            insertion.setBorder(new LineBorder(Color.BLACK, 3));
+            bubble.setBorder(new LineBorder(Color.BLACK, 3));
+            quick.setBorder(new LineBorder(Color.BLACK, 3));
+            merge.setBorder(new LineBorder(Color.BLACK, 3));
+            selection.setBorder(new LineBorder(Color.BLACK, 3));
+            shell.setBorder(new LineBorder(Color.BLACK, 3));
+            stop.setBorder(new LineBorder(Color.BLACK, 3));
+            
+            shuffle.setBackground(Color.BLUE);
+           insertionDec.setBackground(Color.BLUE);
+           bubbleDec.setBackground(Color.BLUE);
+           quickDec.setBackground(Color.BLUE);
+           mergeDec.setBackground(Color.BLUE);
+           selectionDec.setBackground(Color.BLUE);
+           shellDec.setBackground(Color.BLUE);
+            insertion.setBackground(Color.BLUE);
+            bubble.setBackground(Color.BLUE);
+            quick.setBackground(Color.BLUE);
+            merge.setBackground(Color.BLUE);
+            selection.setBackground(Color.BLUE);
+            shell.setBackground(Color.BLUE);
+            stop.setBackground(Color.BLUE);  
+            panel.repaint();
         });
         
         theme4.addActionListener((ActionEvent e) -> {
@@ -381,6 +543,36 @@ public class Mypanel{
             isTheme1 = false;
             isTheme2 = false;
             isTheme3 = false;
+           shuffle.setBorder(new LineBorder(Color.BLACK, 3));
+           insertionDec.setBorder(new LineBorder(Color.BLACK, 3));
+           bubbleDec.setBorder(new LineBorder(Color.BLACK, 3));
+           quickDec.setBorder(new LineBorder(Color.BLACK, 3));
+           mergeDec.setBorder(new LineBorder(Color.BLACK, 3));
+           selectionDec.setBorder(new LineBorder(Color.BLACK, 3));
+           shellDec.setBorder(new LineBorder(Color.BLACK, 3));
+            insertion.setBorder(new LineBorder(Color.BLACK, 3));
+            bubble.setBorder(new LineBorder(Color.BLACK, 3));
+            quick.setBorder(new LineBorder(Color.BLACK, 3));
+            merge.setBorder(new LineBorder(Color.BLACK, 3));
+            selection.setBorder(new LineBorder(Color.BLACK, 3));
+            shell.setBorder(new LineBorder(Color.BLACK, 3));
+            stop.setBorder(new LineBorder(Color.BLACK, 3));
+
+          shuffle.setBackground(new Color(138, 43, 226));
+           insertionDec.setBackground(new Color(138, 43, 226));
+           bubbleDec.setBackground(new Color(138, 43, 226));
+           quickDec.setBackground(new Color(138, 43, 226));
+           mergeDec.setBackground(new Color(138, 43, 226));
+           selectionDec.setBackground(new Color(138, 43, 226));
+           shellDec.setBackground(new Color(138, 43, 226));
+            insertion.setBackground(new Color(138, 43, 226));
+            bubble.setBackground(new Color(138, 43, 226));
+            quick.setBackground(new Color(138, 43, 226));
+            merge.setBackground(new Color(138, 43, 226));
+            selection.setBackground(new Color(138, 43, 226));
+            shell.setBackground(new Color(138, 43, 226));  
+            stop.setBackground(new Color(138, 43, 226));  
+            panel.repaint();
         });
         changeThemes.setMnemonic(KeyEvent.VK_1);
         theme1.setMnemonic(KeyEvent.VK_R);
